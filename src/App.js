cd2353085ecc1f0ad54ae4091481b2cd3842b0e8
becca <rebecca.kaci@gmail.com>
2021-10-26 18:01:43 +0200

    bonus 2 avec seulement 2 fonctions terminé

--- src/App.js
+++ src/App.js
@@ -6,18 +6,35 @@ class App extends React.Component {
     super()
 
     this.state = {
-      count: 0
+      count1: 0,
+      count2: 0
     }
   }
 
-  handlePlusClick = () => {
-    if (this.state.count < 100)
-    this.setState ({ count: this.state.count + 1 })
+  // Fonctions pour les boutons +
+  handlePlusClick = (counter) => {
+    if (counter === "count1"){                              // Le compteur 1 ne ne montera jamais au dessus de 100
+      if (this.state.count1 < 100)
+      this.setState ({ count1: this.state.count1 + 1 })
+      if (this.state.count1 === this.state.count2)         // Quand on incrémente le compteur 1 et qu’il a la même valeur que le compteur 2, le compteur 2 s’incrémente aussi
+      this.setState ({count2: this.state.count2 + 1})
+    } else if (counter === "count2"){                      // Le compteur 2 ne ne montera jamais au dessus de 100
+      if (this.state.count2 < 100)
+      this.setState ({ count2: this.state.count2 + 1 })
+    }  
   }
 
-  handleMinusClick = () => {
-    if (this.state.count > 0)
-    this.setState ({ count: this.state.count - 1 })
+  // Fonction pour les boutons -
+  handleMinusClick = (counter) => {                           // Le compteur 1 ne descendra jamais en dessous de 0
+    if (counter === "count1"){
+      if (this.state.count1 > 0)                              
+      this.setState ({ count1: this.state.count1 - 1 })
+    } else if (counter === "count2"){                         // Le compteur 2 ne descendra jamais en dessous de 0
+      if (this.state.count2 > 0)
+      this.setState ({ count2: this.state.count2 - 1 })
+      if (this.state.count2 === this.state.count1)            // Quand on décrémente le compteur 2 et qu’il a la même valeur que le compteur 1, le compteur 1 se décrémente aussi
+      this.setState ({count1: this.state.count1 -1})
+    }
   }
 
   render() {
@@ -25,8 +42,8 @@ class App extends React.Component {
     return (
       <div className="card border border-dark border-4 rounded shadow m-3 p-2">
         <h1 className="text-center rounded text-white bg-dark p-2">Counter</h1>
-        <Counter count={this.state.count} substract={this.handleMinusClick} increment={this.handlePlusClick}/>
-        <Counter count={this.state.count} substract={this.handleMinusClick} increment={this.handlePlusClick}/>
+        <Counter count={this.state.count1} choice="count1" substract={this.handleMinusClick} increment={this.handlePlusClick}/>
+        <Counter count={this.state.count2} choice="count2" substract={this.handleMinusClick} increment={this.handlePlusClick}/>
       </div>
     )
   }
@@ -34,3 +51,4 @@ class App extends React.Component {
 }
 
 export default App;
+
--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -2,15 +2,22 @@ import React from 'react';
 
 class Counter extends React.Component {
     constructor(){
-        super()
+        super() // permet de récupérer les props
     }
 
     render(){
+        console.log(this.props)
         return(
             <div className="d-flex justify-content-center align-items-center">
-                <button className="btn btn-outline-success m-4 rounded-circle" onClick={this.props.substract}>-</button>
+                <button 
+                    className="btn btn-outline-success m-4 rounded-circle" 
+                    onClick={() => this.props.substract(this.props.choice)}
+                >-</button>
                 <h2>{this.props.count}</h2>
-                <button className="btn btn-outline-danger m-4 rounded-circle" onClick={this.props.increment}>+</button>
+                <button 
+                    className="btn btn-outline-danger m-4 rounded-circle" 
+                    onClick={() => this.props.increment(this.props.choice)}
+                >+</button>
             </div>
         )
     }
